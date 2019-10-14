d17fea9501aa41908afe85683b76dc7d3ef03ea3
Bernardas <bkukulskis49@gmail.com>
2019-10-14 15:39:12 +0300

    Made window movable

--- WindowsFormsApp15/view/MainWindow.cs
+++ WindowsFormsApp15/view/MainWindow.cs
@@ -215,5 +215,37 @@ namespace WindowsFormsApp15.view
             SignUpWindow suw = new SignUpWindow();
             suw.Show();
         }
+        Point lastPoint;
+        private void topHeaderPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void topHeaderPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            {
+                lastPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void logoPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            {
+                lastPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void logoPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
     }
 }
