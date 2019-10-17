fef1bb97b9e16dcfb202fe0b11bdb94342f10444
Jan Wehner <jan.wehner@stud.uni-bamberg.de>
2019-10-17 16:20:31 +0300

    Check if user has already rated course.

--- WindowsFormsApp15/view/MainWindow.Designer.cs
+++ WindowsFormsApp15/view/MainWindow.Designer.cs
@@ -427,7 +427,7 @@
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F); 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 481);
             this.Controls.Add(this.overallSearchPanel);
